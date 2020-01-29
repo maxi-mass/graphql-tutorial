f2bfdb65ce7da48f8ff997a0a6e6e16107bb86a9
Maxim Svistunov <svistunovms@lad24.ru>
2020-01-29 14:54:40 +0300

    Added root query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,5 @@
 const graphql = require('graphql')
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
 const MovieType = new GraphQLObjectType({
   name: 'Movie',
@@ -12,11 +12,17 @@ const MovieType = new GraphQLObjectType({
 
 const Query = new GraphQLObjectType({
   name: 'Query',
-  movie: {
-    type: MovieType,
-    args: { id: { type: GraphQLString } },
-    resolve(parent, args) {
+  fields: {
+    movie: {
+      type: MovieType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
 
+      }
     }
   }
+})
+
+module.exports = new GraphQLSchema({
+  query: Query
 })
