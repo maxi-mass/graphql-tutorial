ac3d5f18a0201b8c309212476dc81baf15776e02
Maxim Svistunov <svistunovms@lad24.ru>
2020-01-29 14:07:46 +0300

    root query. In progress

---
+++ server/schema/schema.js
@@ -0,0 +1,22 @@
+const graphql = require('graphql')
+const { GraphQLObjectType, GraphQLString } = graphql;
+
+const MovieType = new GraphQLObjectType({
+  name: 'Movie',
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString }
+  })
+})
+
+const Query = new GraphQLObjectType({
+  name: 'Query',
+  movie: {
+    type: MovieType,
+    args: { id: { type: GraphQLString } },
+    resolve(parent, args) {
+
+    }
+  }
+})
