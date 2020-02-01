ac3685e7b93c9c8450a51ff25126e02099e87e00
Maximas <ma-xsoul@yandex.ru>
2020-02-01 07:15:16 +0300

    Migrate data to mongo DB. And create connection to DB

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,27 +1,37 @@
-const graphql = require('graphql')
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList } = graphql;
+const graphql = require("graphql");
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLList
+} = graphql;
 
+const Movies = require("../models/movie");
+const Directors = require("../models/director");
+/*
 const movies = [
-  { id: 1, name: 'Film 1', genre: 'Crime', directorId: 1 },
-  { id: 2, name: 'Film 2', genre: 'Sci-Fi', directorId: 2 },
-  { id: 3, name: 'Film 3', genre: 'Sci-Fi-Triller', directorId: 3 },
-  { id: 4, name: 'Film 4', genre: 'Crime-Comedy', directorId: 4 },
-  { id: 5, name: 'Film 5', genre: 'Comedy', directorId: 5 },
-  { id: 6, name: 'Film 6', genre: 'Crime-Comedy', directorId: 1 },
-  { id: 7, name: 'Film 7', genre: 'Sci', directorId: 1 },
-  { id: 7, name: 'Film 8', genre: 'Detective', directorId: 4 }
-]
+  { id: 1, name: "Film 1", genre: "Crime", directorId: 1 },
+  { id: 2, name: "Film 2", genre: "Sci-Fi", directorId: 2 },
+  { id: 3, name: "Film 3", genre: "Sci-Fi-Triller", directorId: 3 },
+  { id: 4, name: "Film 4", genre: "Crime-Comedy", directorId: 4 },
+  { id: 5, name: "Film 5", genre: "Comedy", directorId: 5 },
+  { id: 6, name: "Film 6", genre: "Crime-Comedy", directorId: 1 },
+  { id: 7, name: "Film 7", genre: "Sci", directorId: 1 },
+  { id: 7, name: "Film 8", genre: "Detective", directorId: 4 }
+];
 
 const directors = [
-  { id: 1, name: 'Quentin', age: 55 },
-  { id: 2, name: 'Michael', age: 72 },
-  { id: 3, name: 'James', age: 51 },
-  { id: 4, name: 'Guy', age: 50 },
-  { id: 5, name: 'Federico', age: 70 }
-]
-
+  { id: 1, name: "Quentin", age: 55 },
+  { id: 2, name: "Michael", age: 72 },
+  { id: 3, name: "James", age: 51 },
+  { id: 4, name: "Guy", age: 50 },
+  { id: 5, name: "Federico", age: 70 }
+];
+*/
 const MovieType = new GraphQLObjectType({
-  name: 'Movie',
+  name: "Movie",
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
@@ -30,11 +40,12 @@ const MovieType = new GraphQLObjectType({
     director: {
       type: DirectorType,
       resolve(parent, args) {
-        return directors.find(director => director.id == parent.directorId)
+        return Directors.findById(parent.directorId);
+        //return directors.find(director => director.id == parent.directorId);
       }
     }
   })
-})
+});
 
 const DirectorType = new GraphQLObjectType({
   name: "Director",
@@ -45,44 +56,49 @@ const DirectorType = new GraphQLObjectType({
     movies: {
       type: new GraphQLList(MovieType),
       resolve(parent, args) {
-        return movies.filter(movie => movie.directorId == parent.id)
+        return Movies.find({ directorId: parent.id });
+        //return movies.filter(movie => movie.directorId == parent.id);
       }
     }
   })
-})
+});
 
 const Query = new GraphQLObjectType({
-  name: 'Query',
+  name: "Query",
   fields: {
     movie: {
       type: MovieType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return movies.find(movie => movie.id == args.id)
+        return Movies.findById(args.id);
+        //return movies.find(movie => movie.id == args.id);
       }
     },
     director: {
       type: DirectorType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return directors.find(director => director.id == args.id)
+        return Directors.findById(args.id);
+        //return directors.find(director => director.id == args.id);
       }
     },
     movies: {
       type: new GraphQLList(MovieType),
       resolve(parent, args) {
-        return movies
+        return Movies.find({});
+        //return movies;
       }
     },
     directors: {
       type: new GraphQLList(DirectorType),
       resolve(parent, args) {
-        return directors
+        return Directors.find({});
+        //return directors;
       }
     }
   }
-})
+});
 //mongodb+srv://maximas:maxsoul1@cluster-graphql-tutorial-xnosu.mongodb.net/test?retryWrites=true&w=majority
 module.exports = new GraphQLSchema({
   query: Query
-})
+});
--- server/server/app.js
+++ server/server/app.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const graphqlHTTP = require('express-graphql')
-const schema = require('../schema/schema')
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("../schema/schema");
+const mongoose = require("mongoose");
 
-const app = express()
-const PORT = 3005
-app.use('/graphql', graphqlHTTP({ schema, graphiql: true }))
+const app = express();
+const PORT = 3005;
+
+mongoose.connect(
+  "mongodb+srv://maximas:maximas@cluster0-rgep7.mongodb.net/graphql",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+  // { useMongoClient: true }
+);
+
+app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
+
+const dbConnection = mongoose.connection;
+dbConnection.on("error", err => console.log(`Connection error ${err}`));
+dbConnection.once("open", () => console.log("Connected to DB!"));
 
 app.listen(PORT, err => {
-  err ? console.log(err) : console.log("Server started...")
-})
+  err ? console.log(err) : console.log("Server started...");
+});
